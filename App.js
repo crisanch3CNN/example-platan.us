55853125f0db1867f30a1afb24a10632332d088e
crisanch <cristian.sanchez@triciclos.cl>
2021-12-28 17:54:20 -0300

    feat(show-age): add calcutation age

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react'
-import { TextInput, View, SafeAreaView, Button } from 'react-native';
+import { Text, TextInput, View, SafeAreaView, Button, Alert } from 'react-native';
 import tailwind from 'tailwind-rn';
+import axios from 'axios';
 
 
 export default function App() {
 
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState('');
+  const [age, setAge] = useState(null);
+
+  async function handleClick() {
+    // Alert.alert('Hola');
+    const { data } = await axios.get(`https://api.agify.io/?name=${value}`);
+    setAge(data.age);
+
+  }
 
   return (
     <SafeAreaView style={tailwind('bg-black flex-1')}>
@@ -17,8 +26,10 @@ export default function App() {
             onChangeText={(newValue) => { setValue(newValue) }}
             style={tailwind('bg-gray-100 p-4 flex-1 mr-2')}
           />
-          <Button title="Calcular" />
+          <Button title="Calcular" onPress={handleClick} />
         </View>
+        <Text style={tailwind('text-white mt-2')} >Tu edad aproximada es ....</Text>
+        <Text style={tailwind('text-white text-lg')}>{age}</Text>
 
         <StatusBar style="light" />
       </View>
